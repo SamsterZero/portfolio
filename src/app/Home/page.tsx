85ccaa7cb4e8edf71b64d814fa00f1d064371c52
Vinay Mhatre <90505086+SamsterZero@users.noreply.github.com>
2024-12-26 02:23:58 +0530

    changed import

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,6 +1,6 @@
 import EducationSection from "@/components/Education";
 import MatrixRain from "@/components/MatrixRain";
-import Profile from "@/components/Profile";
+import Profile from "@/components/profile";
 import SkillSection from "@/components/SkillSection";
 import WorkExperience from "@/components/WorkExperience";
 
